7fe74f6971be12aec796008391213d7015bcba68
markvzmiller <markvzmiller@gmail.com>
2024-02-08 19:38:51 -0500

    Updated message

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Add a to do item above!</p>
+                <p className="text-center">You have no to do items.  Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
